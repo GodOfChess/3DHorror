782b5878fc982efc7acb433048596ed73f814692
GodOfChess <CreativeOrange1@yandex.ru>
2021-11-08 21:36:48 +0500

    Импорт ассетов
    Импорт ассетов, создание локации для кат-сцены

---
+++ Assets/Scripts/CutScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutScene : MonoBehaviour
+{
+    [SerializeField] float speed = 15f;
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
