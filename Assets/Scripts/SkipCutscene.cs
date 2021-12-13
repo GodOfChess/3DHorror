f6d861027626ddf493675213b031c3a7901d676d
GodOfChess <CreativeOrange1@yandex.ru>
2021-12-13 19:15:52 +0500

    Оптимизация игры
    Переделан террейн
    Добавлен фонарик
    Настроено взаимодействие фонарика и игрока

--- Assets/Scripts/SkipCutscene.cs
+++ Assets/Scripts/SkipCutscene.cs
@@ -4,20 +4,12 @@ using UnityEngine;
 
 public class SkipCutscene : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject cameraFirst, cameraSecond, mainCamera;
+
     void Start()
     {
-        GameObject camera1 = GameObject.Find("Camera1");
-        camera1.SetActive(false);
-        GameObject camera2 = GameObject.Find("Camera2");
-        camera2.SetActive(false);
-        GameObject mainCamera = GameObject.Find("PlayerCamera");
+        cameraFirst.SetActive(false);
+        cameraSecond.SetActive(false);
         mainCamera.SetActive(true);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
